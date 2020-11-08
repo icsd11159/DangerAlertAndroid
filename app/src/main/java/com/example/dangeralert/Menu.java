5fea2e427b1b94bafc6a86aad5aeab56d1393b64
icsd11159 <marthatodi92@gmail.com>
2020-11-09 00:42:49 +0200

    AddScrollbar

--- app/src/main/java/com/example/dangeralert/Menu.java
+++ app/src/main/java/com/example/dangeralert/Menu.java
@@ -30,6 +30,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
